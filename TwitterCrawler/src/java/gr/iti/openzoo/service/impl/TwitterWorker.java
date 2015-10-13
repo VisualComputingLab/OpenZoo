abbd3d15a179e2c4b83074531bca418fad5f1e7c
Dimitris Samaras <dimitris.samaras1@gmail.com>
2015-10-13 10:21:31 +0300

    Twitter Crawler for openzoo
    Reverted changes

--- TwitterCrawler/src/java/gr/iti/openzoo/service/impl/TwitterWorker.java
+++ TwitterCrawler/src/java/gr/iti/openzoo/service/impl/TwitterWorker.java
@@ -4,12 +4,10 @@ import gr.iti.openzoo.admin.Message;
 import gr.iti.openzoo.impl.OpenZooLoggingConnection;
 import gr.iti.openzoo.impl.OpenZooLoneWorker;
 import gr.iti.openzoo.impl.OpenZooOutputConnection;
-import java.util.Arrays;
 import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 import twitter4j.FilterQuery;
-import twitter4j.GeoLocation;
 import twitter4j.Status;
 import twitter4j.StatusDeletionNotice;
 import twitter4j.StatusListener;
@@ -25,266 +23,183 @@ import twitter4j.json.DataObjectFactory;
  */
 public class TwitterWorker extends OpenZooLoneWorker {
 
-    private static String OP_COMMAND_KEYWORDS = "keywords";
-    private static String OP_COMMAND_LOCATION = "location";
     private OpenZooOutputConnection outConn = new OpenZooOutputConnection(this, "tw_output");
     private OpenZooLoggingConnection logConn = new OpenZooLoggingConnection(this);
-
-    public TwitterWorker(String threadName) {
+    
+    public TwitterWorker(String threadName)
+    {        
         super(threadName);
-
+        
         log.debug("-- TwitterWorker()");
     }
-
+    
     @Override
     public boolean doWork(Message message) {
-
+        
         return true;
     }
-
-    private Configuration createConfiguration(String consumerKey, String consumerSecret, String accessToken, String accessTokenSecret) {
-        ConfigurationBuilder confBuilder = new ConfigurationBuilder();
-
+    
+    private Configuration createConfiguration(String consumerKey, String consumerSecret, String accessToken, String accessTokenSecret)
+    {
+        ConfigurationBuilder confBuilder = new ConfigurationBuilder();		
+			
         confBuilder.setDebugEnabled(true);
         confBuilder.setOAuthConsumerKey(consumerKey);
         confBuilder.setOAuthConsumerSecret(consumerSecret);
         confBuilder.setOAuthAccessToken(accessToken);
         confBuilder.setOAuthAccessTokenSecret(accessTokenSecret);
         confBuilder.setJSONStoreEnabled(true);
-
-
+        
+        
         return confBuilder.build();
     }
-
+    
     @Override
-    public void run() {
+    public void run()
+    {
         log.debug("-- TwitterWorker.run");
-
-        if (!logConn.init()) {
+        
+        if (!logConn.init())
+        {
             log.error("Error by endpoint initialization");
             return;
         }
-
+        
         logConn.debug("Running...");
-
-        if (!outConn.init()) {
+        
+        if (!outConn.init())
+        {
             log.error("Error by endpoint initialization");
             logConn.error("Error by endpoint initialization");
             return;
         }
-
+        
         // Access here the required parameters
         String conKey = getRequiredParameter("consumerKey");
         String conSec = getRequiredParameter("consumerSecret");
         String accTok = getRequiredParameter("accessToken");
         String accTokSec = getRequiredParameter("accessTokenSecret");
-        String operation = getRequiredParameter("search_by");
-
+        JSONArray keywords;
+        try
+        {
+            keywords = new JSONArray(getRequiredParameter("keywords"));
+        }
+        catch (JSONException e)
+        {
+            log.error("JSONException while reading keywords from KV: " + e);
+            logConn.error("JSONException while reading keywords from KV: " + e);
+            return;
+        }
+        
+        log.info("Read required parameters from KV: " + conKey + " " + conSec + " " + accTok + " " + accTokSec + " " + keywords);
+        logConn.info("Read required parameters from KV: " + conKey + " " + conSec + " " + accTok + " " + accTokSec + " " + keywords);
+        
         Configuration config = createConfiguration(conKey, conSec, accTok, accTokSec);
-        TwitterStream twitterStream = new TwitterStreamFactory(config).getInstance();
-
-        if (operation.equals(OP_COMMAND_KEYWORDS)) {
-
-            JSONArray keywords;
-
-            try {
-                keywords = new JSONArray(getRequiredParameter("keywords"));
-            } catch (JSONException e) {
-                log.error("JSONException while reading keywords from KV: " + e);
-                logConn.error("JSONException while reading keywords from KV: " + e);
-                return;
-            }
-
-            log.info("Read required parameters from KV: " + conKey + " " + conSec + " " + accTok + " " + accTokSec + " " + keywords);
-            logConn.info("Read required parameters from KV: " + conKey + " " + conSec + " " + accTok + " " + accTokSec + " " + keywords);
-
-            if (keywords.length() == 0) {
-                log.error("No keywords given, aborting");
-                logConn.error("No keywords given, aborting");
-                return;
-            }
-
-            String allkwordinastring = "";
-            String tmps;
-            String[] kwords = new String[keywords.length()];
-            for (int n = 0; n < keywords.length(); n++) {
-                tmps = keywords.optString(n);
-                kwords[n] = tmps;
-                allkwordinastring += tmps + ":";
-            }
-
-            // create twitter stream and listener
-
-            final String[] kwordarray = kwords;
-
-            StatusListener listenerKW = new StatusListener() {
-                //When a tweet, that contains at least one of the queries, is uploaded:
-                @Override
-                public void onStatus(Status status) {
-                    String statusString = DataObjectFactory.getRawJSON(status);
-
-                    try {
-                        JSONArray jsonarr = new JSONArray();
-                        for (int k = 0; k < kwordarray.length; k++) {
-                            if (status.getText().toLowerCase().indexOf(kwordarray[k]) >= 0) {
-                                jsonarr.put(kwordarray[k]);
-                            }
-                        }
-
-                        JSONObject json = new JSONObject(statusString);
-                        json.put("keywords", jsonarr);
-
-                        Message message = createEmptyMessage();
-
-                        message.setPayload(json);
-
-                        //boolean success = doWork(message);
-
-                        message.setSuccess(true);
-
-                        outConn.put(message);
-
-                    } catch (JSONException ex) {
-                        log.error("JSONException during sending twitter message to rabbitmq: " + ex);
-                        logConn.error("JSONException during sending twitter message to rabbitmq: " + ex);
-                    }
-                }
-
-                @Override
-                public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
-                }
-
-                @Override
-                public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
-                    log.error("onTrackLimitationNotice in StatusListener: numberOfLimitedStatuses = " + numberOfLimitedStatuses);
-                    logConn.error("onTrackLimitationNotice in StatusListener: numberOfLimitedStatuses = " + numberOfLimitedStatuses);
-                }
-
-                @Override
-                public void onScrubGeo(long userId, long upToStatusId) {
-                }
-
-                @Override
-                public void onException(Exception ex) {
-                    log.error("Exception in StatusListener: " + ex);
-                }
-            };
-
-            log.info("Born!");
-            logConn.info("Born!");
-
-
-            twitterStream.addListener(listenerKW);
-            FilterQuery fq = new FilterQuery();
-            fq.setIncludeEntities(true);
-
-            fq.track(kwordarray);
-            log.info("I will listen for the following keywords: " + allkwordinastring);
-            logConn.info("I will listen for the following keywords: " + allkwordinastring);
-
-            twitterStream.filter(fq);
-
-
-        } else if (operation.equals(OP_COMMAND_LOCATION)) {
-
-            double lon1 = Double.parseDouble(getRequiredParameter("lon1"));
-            double lat1 = Double.parseDouble(getRequiredParameter("lat1"));
-            double lon2 = Double.parseDouble(getRequiredParameter("lon2"));
-            double lat2 = Double.parseDouble(getRequiredParameter("lat2"));
-
-            double bbox[][] = {{lon1, lat1}, {lon2, lat2}};
-
-            log.info("Read required parameters from KV: " + conKey + " " + conSec + " " + accTok + " " + accTokSec + " " + Arrays.toString(bbox));
-            logConn.info("Read required parameters from KV: " + conKey + " " + conSec + " " + accTok + " " + accTokSec + " " + Arrays.toString(bbox));
-
-            if (bbox == null && bbox.length < 4) {
-                log.error("No coordinates given, aborting");
-                logConn.error("No coordinates given, aborting");
-                return;
-            }
-
-            // create twitter stream and listener
-
-
-            StatusListener listenerLOC = new StatusListener() {
-                //When a tweet, that contains at least one of the queries, is uploaded:
-                @Override
-                public void onStatus(Status status) {
-                    String statusString = DataObjectFactory.getRawJSON(status);
-
-
-                    try {
-                        GeoLocation gl = status.getGeoLocation();
-                        StringBuilder msg = new StringBuilder();
-                        if (gl != null) {
-                            msg.append("Lat/Lon: ").append(gl.getLatitude()).append(",").append(gl.getLongitude()).append(" - ");
-                            msg.append(status.getText());
-
-                            JSONObject json = new JSONObject(statusString);
-                            json.put("location", msg);
-
-                            Message message = createEmptyMessage();
-
-                            message.setPayload(json);
-
-                            //boolean success = doWork(message);
-
-                            message.setSuccess(true);
-
-                            outConn.put(message);
-                        }
-                    } catch (JSONException ex) {
-                        log.error("JSONException during sending twitter message to rabbitmq: " + ex);
-                        logConn.error("JSONException during sending twitter message to rabbitmq: " + ex);
-                    }
-                }
-
-                @Override
-                public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
-                }
-
-                @Override
-                public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
-                    log.error("onTrackLimitationNotice in StatusListener: numberOfLimitedStatuses = " + numberOfLimitedStatuses);
-                    logConn.error("onTrackLimitationNotice in StatusListener: numberOfLimitedStatuses = " + numberOfLimitedStatuses);
-                }
-
-                @Override
-                public void onScrubGeo(long userId, long upToStatusId) {
-                }
-
-                @Override
-                public void onException(Exception ex) {
-                    log.error("Exception in StatusListener: " + ex);
-                }
-            };
-
-            log.info("Born!");
-            logConn.info("Born!");
-
-
-            twitterStream.addListener(listenerLOC);
-            FilterQuery fq = new FilterQuery();
-            fq.setIncludeEntities(true);
-
-            fq.locations(bbox);
-            log.info("I will listen for the following location: " + Arrays.toString(bbox));
-            logConn.info("I will listen for the following location: " + Arrays.toString(bbox));
-
-            twitterStream.filter(fq);
-
+        
+        if (keywords.length() == 0)
+        {
+            log.error("No keywords given, aborting");
+            logConn.error("No keywords given, aborting");
+            return;
         }
 
+        String allkwordinastring = "";
+        String tmps;
+        String [] kwords = new String[keywords.length()];
+        for (int n = 0; n < keywords.length(); n++)
+        {
+            tmps = keywords.optString(n);
+            kwords[n] = tmps;
+            allkwordinastring += tmps + ":";
+        }
+        
+        // create twitter stream and listener
 
-        while (!enough) {
-            try {
+        final String [] kwordarray = kwords;
+
+        StatusListener listener = new StatusListener() 
+        {   
+            //When a tweet, that contains at least one of the queries, is uploaded:
+            @Override
+            public void onStatus(Status status) 
+            {
+                String statusString = DataObjectFactory.getRawJSON(status);
+
+                try 
+                {
+                    JSONArray jsonarr = new JSONArray();
+                    for (int k = 0; k < kwordarray.length; k++)
+                    {
+                        if (status.getText().toLowerCase().indexOf(kwordarray[k]) >= 0)
+                            jsonarr.put(kwordarray[k]);
+                    }
+
+                    JSONObject json = new JSONObject(statusString);
+                    json.put("keywords", jsonarr);
+                    
+                    Message message = createEmptyMessage();
+                    
+                    message.setPayload(json);
+            
+                    //boolean success = doWork(message);
+
+                    message.setSuccess(true);
+
+                    outConn.put(message);
+                    
+                } 
+                catch (JSONException ex) 
+                {
+                    log.error("JSONException during sending twitter message to rabbitmq: " + ex);
+                    logConn.error("JSONException during sending twitter message to rabbitmq: " + ex);
+                } 
+            }
+
+            @Override
+            public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {}
+            @Override
+            public void onTrackLimitationNotice(int numberOfLimitedStatuses)
+            {
+                log.error("onTrackLimitationNotice in StatusListener: numberOfLimitedStatuses = " + numberOfLimitedStatuses);
+                logConn.error("onTrackLimitationNotice in StatusListener: numberOfLimitedStatuses = " + numberOfLimitedStatuses);
+            }
+            @Override
+            public void onScrubGeo(long userId, long upToStatusId) {}
+            @Override
+            public void onException(Exception ex)
+            {
+                log.error("Exception in StatusListener: " + ex);
+            }
+        };
+
+        log.info("Born!");
+        logConn.info("Born!");
+        
+        TwitterStream twitterStream = new TwitterStreamFactory(config).getInstance();
+        twitterStream.addListener(listener);
+        FilterQuery fq = new FilterQuery();
+        fq.setIncludeEntities(true);
+
+        fq.track(kwordarray);
+        log.info("I will listen for the following keywords: " + allkwordinastring);
+        logConn.info("I will listen for the following keywords: " + allkwordinastring);
+
+        twitterStream.filter(fq);
+        
+                
+        while (!enough) 
+        {
+            try
+            {
                 Thread.sleep(5000);
-            } catch (InterruptedException e) {
+            }
+            catch (InterruptedException e)
+            {
                 log.info("Worker woke up");
                 logConn.info("Worker woke up");
             }
         }
-
+        
         log.info("Cleaning up twitter stream");
         logConn.info("Cleaning up twitter stream");
         twitterStream.cleanUp();
